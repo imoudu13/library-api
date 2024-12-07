943596c7568ac2c94a333c95d5dc045510dd9a84
imoudu13 <imouduibrahim11@gmail.com>
2024-12-07 14:12:52 -0800

    removed unneccesary dependency

--- src/main/java/com/moducation/library/api/repositories/UserRepository.java
+++ src/main/java/com/moducation/library/api/repositories/UserRepository.java
@@ -3,9 +3,8 @@ package com.moducation.library.api.repositories;
 import com.moducation.library.api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUsername(String username);
-    public User findByEmail(String email);
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
