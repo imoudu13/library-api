a5afbb7e08f1563b268e6d9e3507ce423d6ad646
imoudu13 <imouduibrahim11@gmail.com>
2024-12-08 15:33:02 -0800

    created exception for incorrect filter

---
+++ src/main/java/com/moducation/library/api/exceptions/IncorrectFilterException.java
@@ -0,0 +1,7 @@
+package com.moducation.library.api.exceptions;
+
+public class IncorrectFilterException extends Exception {
+    public IncorrectFilterException(String message) {
+        super(message);
+    }
+}
