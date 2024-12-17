82275206afa76c5a7146734e4575d4dcb4182ad7
imoudu13 <imouduibrahim11@gmail.com>
2024-12-17 00:11:00 -0800

    added variables for withdrawal and return

--- src/main/java/com/moducation/library/api/utils/Constants.java
+++ src/main/java/com/moducation/library/api/utils/Constants.java
@@ -3,4 +3,7 @@ package com.moducation.library.api.utils;
 public final class Constants {
     private Constants() {}
     public static final Long ONE_WEEK_IN_MILLIS = 604_800_000L;
+    public static final Integer WITHDRAWAL_CODE = 1;
+    public static final Integer RETURN_CODE = 2;
+    // public static final Integer RESERVE_CODE = 3;
 }
