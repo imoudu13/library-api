0f87313588daabb1743b5cd2bb9f121d542a8c8b
imoudu13 <imouduibrahim11@gmail.com>
2024-12-18 19:15:52 -0800

    fixed the queries in the repositories

--- src/main/java/com/moducation/library/api/repositories/BookActivityHistoryRepository.java
+++ src/main/java/com/moducation/library/api/repositories/BookActivityHistoryRepository.java
@@ -1,8 +1,17 @@
 package com.moducation.library.api.repositories;
 
+import com.moducation.library.api.models.Book;
 import com.moducation.library.api.models.BookActivityHistory;
+import com.moducation.library.api.models.LibraryUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BookActivityHistoryRepository extends JpaRepository<BookActivityHistory, Long> {}
+public interface BookActivityHistoryRepository extends JpaRepository<BookActivityHistory, Long> {
+    @Query("SELECT b FROM BookActivityHistory b WHERE b.libraryUser = :libraryUser AND b.book = :book AND b.type = :type ORDER BY b.date DESC LIMIT 1")
+    BookActivityHistory findBookActivityForWithdrawal(@Param("type") Integer type,
+                                                      @Param("libraryUser") LibraryUser libraryUser,
+                                                      @Param("book") Book book);
+}
--- src/main/java/com/moducation/library/api/repositories/BookWithdrawalRepository.java
+++ src/main/java/com/moducation/library/api/repositories/BookWithdrawalRepository.java
@@ -1,9 +1,16 @@
 package com.moducation.library.api.repositories;
 
+import com.moducation.library.api.models.BookActivityHistory;
 import com.moducation.library.api.models.BookWithdrawal;
+import com.moducation.library.api.models.LibraryUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BookWithdrawalRepository extends JpaRepository<BookWithdrawal, Long> {
+
+    @Query("SELECT bw FROM BookWithdrawal bw WHERE bw.bookActivity = :bookActivityHistory AND bw.libraryUser = :libraryUser")
+    BookWithdrawal findByBookActivityHistoryAndLibraryUser(@Param("bookActivityHistory") BookActivityHistory bookActivityHistory, @Param("libraryUser") LibraryUser libraryUser);
 }
